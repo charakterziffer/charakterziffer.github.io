a442eb7a0e7ac74eb838ba5f871e14a8c46ea7ff
Gerhard Großmann <postfach2b@web.de>
2014-05-11 08:23:07 +0200

    beta löschen

--- beta/bestand/parallax.js
+++
@@ -1,3 +0,0 @@
-if (window.innerWidth > 940) {
-window.onscroll=function(){var speed=3.5;document.body.style.backgroundPosition=(-window.pageXOffset/speed)+"px "+(-window.pageYOffset/speed)+"px";}
-}
