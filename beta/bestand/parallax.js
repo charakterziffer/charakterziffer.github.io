f69fcc0f6309d84d0ec5e2f793a027e286d31920
Gerhard Großmann <postfach2b@web.de>
2014-05-11 12:02:16 +0200

    Test der Kommentarfunktion

---
+++ beta/bestand/parallax.js
@@ -0,0 +1,3 @@
+if (window.innerWidth > 940) {
+window.onscroll=function(){var speed=3.5;document.body.style.backgroundPosition=(-window.pageXOffset/speed)+"px "+(-window.pageYOffset/speed)+"px";}
+}
